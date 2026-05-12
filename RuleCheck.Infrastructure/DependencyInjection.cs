04bd94694e24dd6cc0795c4db6d7f7cb775def52
z.mohammadi <z.mohammadi@DESKTOP-79HJ533>
2026-05-12 11:44:36 +0330

    chore: configure DI and register DbContext

---
+++ RuleCheck.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RuleCheck.Infrastructure.Persistence;
+
+namespace RuleCheck.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<RuleCheckDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        return services;
+    }
+}
